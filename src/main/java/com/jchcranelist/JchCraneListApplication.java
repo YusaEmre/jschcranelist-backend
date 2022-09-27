12e37cc349fee26ab98e073185cce9ae11975421
yusaincedere <yusaincedere@gmail.com>
2022-09-27 15:53:27 +0300

    changed app name

---
+++ src/main/java/com/jchcranelist/JchCraneListApplication.java
@@ -0,0 +1,13 @@
+package com.jchcranelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JchCraneListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JchCraneListApplication.class, args);
+	}
+
+}
